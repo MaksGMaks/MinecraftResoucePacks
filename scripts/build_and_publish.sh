575e8a4d36fe416505aa8f85274c3be442092548
kozak <kozakmaksym12@gmail.com>
2026-07-25 12:20:44 +0300

    Initial commit

---
+++ scripts/build_and_publish.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+PACKS=(
+  "packs/mobs.zip"
+  "packs/items.zip"
+)
+BASE_META="packs/base/pack.mcmeta"
+OUT="build/merged_pack"
+ASSET="build/pack.zip"
+
+rm -rf "$OUT" "$ASSET"
+mkdir -p "$OUT"
+
+for p in "${PACKS[@]}"; do
+  unzip -oq "$p" -d "$OUT"
+done
+cp "$BASE_META" "$OUT/pack.mcmeta"
+
+cd "$OUT" && zip -rq "../pack.zip" . && cd ../..
+
+SHA1=$(sha1sum "$ASSET" | awk '{print $1}')
+
+gh release upload "$TAG" "$ASSET" --repo "$REPO" --clobber
+
+echo "Published. Set in server.properties:"
+echo "resource-pack=https://github.com/$REPO/releases/download/$TAG/pack.zip"
+echo "resource-pack-sha1=$SHA1"
