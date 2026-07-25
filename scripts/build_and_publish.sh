7ec14592f266f711f3a19485fca2f31e1ef4262d
kozak <kozakmaksym12@gmail.com>
2026-07-25 14:47:24 +0300

    Fix build issue

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -2,12 +2,14 @@
 set -e
 
 PACKS=(
-  "packs/mobs.zip"
-  "packs/items.zip"
+  "packs/BF5_logo.zip"
+  "packs/BonaFriends 5.zip"
 )
 BASE_META="packs/base/pack.mcmeta"
 OUT="build/merged_pack"
 ASSET="build/pack.zip"
+REPO="MaksGMaks/MinecraftResoucePacks"
+TAG="pack"
 
 rm -rf "$OUT" "$ASSET"
 mkdir -p "$OUT"
